c6d07d4452e6bb112aba52332eb3302b818bf28d
salamafazlul <105430673+salamafazlul@users.noreply.github.com>
2025-09-16 20:19:56 +0530

    Initial commit

---
+++ src/main/java/com/salamafazlul/reactivestudentapi/ReactiveStudentApiApplication.java
@@ -0,0 +1,13 @@
+package com.salamafazlul.reactivestudentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveStudentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveStudentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salamafazlul/reactivestudentapi/ReactiveStudentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salamafazlul.reactivestudentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveStudentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
