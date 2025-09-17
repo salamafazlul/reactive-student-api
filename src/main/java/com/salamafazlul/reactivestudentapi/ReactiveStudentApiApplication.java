e32e3e74e80fe4d1bad46bbe8f295177d0e85513
salamafazlul <105430673+salamafazlul@users.noreply.github.com>
2025-09-17 23:57:18 +0530

    Update ReactiveStudentApiApplication with data seeding CommandLineRunner

--- src/main/java/com/salamafazlul/reactivestudentapi/ReactiveStudentApiApplication.java
+++ src/main/java/com/salamafazlul/reactivestudentapi/ReactiveStudentApiApplication.java
@@ -1,7 +1,11 @@
 package com.salamafazlul.reactivestudentapi;
 
+import com.salamafazlul.reactivestudentapi.student.Student;
+import com.salamafazlul.reactivestudentapi.student.StudentService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ReactiveStudentApiApplication {
@@ -10,4 +14,20 @@ public class ReactiveStudentApiApplication {
 		SpringApplication.run(ReactiveStudentApiApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			StudentService service
+	){
+		return args -> {
+			for (int i = 0; i < 100; i++){
+				service.save(
+						Student.builder()
+								.firstname("John " + i)
+								.lastname("Doe "+ i)
+								.age(i)
+								.build()
+				).subscribe();
+			}
+		};
+	}
 }
