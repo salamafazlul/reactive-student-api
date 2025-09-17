d3c0b4bf1ceeaf18be41e3d7c32433e0851e6a20
salamafazlul <105430673+salamafazlul@users.noreply.github.com>
2025-09-17 23:58:24 +0530

    Add Student entity with Lombok and R2DBC annotations

---
+++ src/main/java/com/salamafazlul/reactivestudentapi/student/Student.java
@@ -0,0 +1,20 @@
+package com.salamafazlul.reactivestudentapi.student;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "students")
+public class Student {
+
+    @Id
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private int age;
+}
